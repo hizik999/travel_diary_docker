616300409a81113cbb9e2be4524ef04fc1e70620
MalyshevAM <com.hizik@gmail.com>
2024-11-07 17:13:12 +0300

    сделал рабочий докер композ

--- dags/mydag2.py
+++ dags/mydag2.py
@@ -4,18 +4,18 @@ from datetime import datetime
 
 with DAG(
     dag_id="test_python",
-    schedule="0 0 * * *",
+    schedule="* * * * *",
     start_date= datetime(2021, 1, 1),
     catchup=False,
 ) as dag:
     run_this = BashOperator(
         task_id="run_after_loop",
-        bash_command="echo HELLO",
+        bash_command="echo HELLO1",
     )
 
     run_this1 = BashOperator(
         task_id="run_after_loop1",
-        bash_command="echo HELLO",
+        bash_command="echo HELLO2",
     )
 
     run_this >> run_this1
